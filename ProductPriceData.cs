1f3b2e92303333f3ac5b376f4bdf777f2a2b8a07
Lillian <fleming.lillian@gmail.com>
2021-04-15 20:39:08 -0500

    Add project files.

---
+++ ProductPriceData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KBeautyPriceScraper
+{
+    internal class ProductPriceData
+    {
+        public decimal ProductPrice { get; set; }
+        public int ProductNumber { get; set; }
+        public string ProductCode { get; set; }
+        public DateTime ViewedDateTime { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KBeautyPriceScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static ProductPriceData GetDataFromUri(Uri uri)
+        {
+            return NotImplementedException()
+        }
+    }
+}
